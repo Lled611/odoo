8659dd8b9d311543098e76ef4028ba43170c2091
Lled611 <sega-sharaev@yandex.ru>
2023-10-12 17:37:11 +0300

    Глава 6

---
+++ technical-training-sandbox/estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'estate',
+    'depends': [
+        'base_setup'
+    ],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml'
+    ]
+}
---
+++ technical-training-sandbox/estate/models/estate_property.py
@@ -0,0 +1,20 @@
+from odoo import models, fields
+
+
+class Property(models.Model):
+    _name = 'estate.property'
+    _description = 'estate property'
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(selection=[('North', 'North'), ('South', 'South'), ('East', 'East'), ('West', 'West')])
