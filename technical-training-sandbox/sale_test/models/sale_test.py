6376ce3125045eea17dd769136ed28730f20c456
Lled611 <sega-sharaev@yandex.ru>
2023-10-18 01:26:40 +0300

    Исправлено второе замечание.

--- technical-training-sandbox/sale_test/models/sale_test.py
+++ technical-training-sandbox/sale_test/models/sale_test.py
@@ -24,13 +24,32 @@ class SaleTest(models.Model):
     def onchange_date_order_amount_total(self):
         if self.date_order:
             self.test = self.new_test(self.amount_total, self.date_order)
+    
+    @staticmethod
+    def get_test_error_message(test):
+        msg = ''
+        if test:
+            if len(test) > 50:
+                msg = _('The length of the text must be less than 50 characters!')
+        return msg
+    
+    @api.onchange('test')
+    def onchange_test(self):
+        error_message = self.get_test_error_message(self.test)
+        if error_message != '':
+            return {
+                'warning': {
+                    'title': _('Warning'),
+                    'message': error_message
+                }
+            }
 
     @api.constrains('test')
     def check_test(self):
         for record in self:
-            if record.test:
-                if len(record.test) > 50:
-                    raise ValidationError(_('The length of the text must be less than 50 characters!'))
+            error_message = self.get_test_error_message(self.test)
+            if error_message != '':
+                raise ValidationError(error_message)
     
     @api.model
     def default_get(self, fields):
